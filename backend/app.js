0fafc0fdc0aec426378f12771c1c1caf4c43b11a
Moeid Haider <moeidhaider9@gmail.com>
2023-10-26 22:33:01 +0100

    minor changes

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const logger = require("morgan");
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
 
 // const postsRouter = require("./routes/posts");
 // const tokensRouter = require("./routes/tokens");
@@ -13,9 +13,9 @@ const app = express();
 // setup for receiving JSON
 app.use(express.json());
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // route setup
 // app.use("/posts", tokenChecker, postsRouter);
@@ -32,10 +32,10 @@ app.use((req, res, next) => {
 app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // respond with details of the error
-  res.status(err.status || 500).json({ message: "server error" });
+  res.status(err.status || 500).json({ message: 'server error' });
 });
 
 module.exports = app;
